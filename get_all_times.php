a61529b0cdb6e845b9af1196cc12770bc55b4c87
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-01-29 15:46:22 +0000

    added <br>

--- get_all_times.php
+++ get_all_times.php
@@ -39,6 +39,7 @@ if (isset($_POST['submit'])) {
   if ($result && $statement->rowCount() > 0) { ?>
     <h2>Results</h2>
 	<?php if ($success) echo $success; ?>
+	<br>
     <table border=1 frame=void rules=all>
       <thead>
 <tr>
