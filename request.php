85c3228780e6f934d3d5394814565ea953b31de5
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-02-15 06:07:47 +0000

    Trying a request thing w/ php

---
+++ request.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+  * Function to query information based on
+  * a parameter: in this case, name.
+  *
+  */
+
+try {
+	require "config.php";
+	require "common.php";
+
+	$connection = new PDO($dsn, $username, $password, $options);
+
+	$sql = str_replace("~", " ", strval($_GET["sql"]))
+
+
+
+	$statement = $connection->prepare($sql);
+	$statement->execute();
+
+	$result = $statement->fetchAll();
+	echo $result;
+} catch(PDOException $error) {
+	echo $sql . "<br>" . $error->getMessage();
+}
+}
+?>
+<?php require "templates/header.php"; ?>
+
+
+<?php require "templates/footer.php"; ?>
