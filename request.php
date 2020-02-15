e9ba6f2ee9d0d3557801d6e29e8e51785f81d634
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-02-15 16:44:35 +0000

    Fun

--- request.php
+++ request.php
@@ -25,7 +25,7 @@ function query_this(string $to_query) {
 }
 
 if($sqlnum == "63610069933") {
-	echo query_this("SELECT * FROM IDENTITY");
+	echo query_this("SELECT IDENTITY.ID, FIRST_NAME, LAST_NAME, SUM(TIMESTAMPDIFF(MINUTE, TIME_IN, TIME_OUT)) AS 'total_min' FROM IDENTITY JOIN PRACTICE ON PRACTICE.ID = IDENTITY.ID WHERE IDENTITY.ID = 1013934 GROUP BY ID;")["total_min"];
 }
 
 ?>
