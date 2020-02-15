4e982499d04a872afc7deb3649c21dd045d79d3b
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-02-15 16:50:50 +0000

    ?xif

--- request.php
+++ request.php
@@ -25,10 +25,14 @@ function query_this(string $to_query) {
 }
 
 if($sqlnum == "63610069933") {
-	$res = query_this("SELECT IDENTITY.ID, FIRST_NAME, LAST_NAME, SUM(TIMESTAMPDIFF(MINUTE, TIME_IN, TIME_OUT)) AS 'total_min' FROM IDENTITY JOIN PRACTICE ON PRACTICE.ID = IDENTITY.ID WHERE IDENTITY.ID = 1013934 GROUP BY ID");
-	foreach ($res as $row) {
-		echo escape($row["total_min"]);
-	}
+	$res = query_this("SELECT IDENTITY.ID, FIRST_NAME, LAST_NAME, SUM(TIMESTAMPDIFF(MINUTE, TIME_IN, TIME_OUT)) AS 'total_min' FROM IDENTITY JOIN PRACTICE ON PRACTICE.ID = IDENTITY.ID WHERE IDENTITY.ID = 1013934");
+	<?php foreach ($result as $row) { ?>
+		<tr>
+	<td><?php echo escape($row["total_min"]);  ?></td>
+
+
+		</tr>
+		<?php } ?>
 }
 
 ?>
