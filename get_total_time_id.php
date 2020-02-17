2b6dcef0a88bf5552ea15d578a1d9e93765cba7f
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-02-17 22:22:47 +0000

    Added Scan Card

--- get_total_time_id.php
+++ get_total_time_id.php
@@ -72,4 +72,81 @@ if (isset($_POST['submit'])) {
 
 <a href="options.php">Back to home</a>
 
+<script>
+Quagga.init({           
+        inputStream : {
+            name : "Live",
+            type : "LiveStream",
+            target: document.querySelector('#barcode-scanner'), 
+             constraints: {
+                width: 100,
+                height: 400,                  
+                facingMode: "environment"  //"environment" for back camera, "user" front camera
+                }               
+        },                         
+        decoder : {
+            readers : ["code_128_reader","code_39_reader"],
+			debug: {
+                        showCanvas: true,
+                        showPatches: true,
+                        showFoundPatches: true,
+                        showSkeleton: true,
+                        showLabels: true,
+                        showPatchLabels: true,
+                        showRemainingPatchLabels: true,
+                        boxFromPatches: {
+                            showTransformed: true,
+                            showTransformedBox: true,
+                            showBB: true
+                        }
+                    }
+        }
+
+    }, function(err) {
+        if (err) {
+            esto.error = err;
+            console.log(err);
+                return
+        }
+
+ 		console.log("Initialization finished. Ready to start");
+        Quagga.start();
+        Quagga.onDetected(function(result) {                              
+                var last_code = result.codeResult.code;                   
+                    console.log("last_code "); 
+             });
+		            Quagga.onProcessed(function (result) {
+                var drawingCtx = Quagga.canvas.ctx.overlay,
+                drawingCanvas = Quagga.canvas.dom.overlay;
+
+                if (result) {
+                    if (result.boxes) {
+                        drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                        result.boxes.filter(function (box) {
+                            return box !== result.box;
+                        }).forEach(function (box) {
+                            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+                        });
+                    }
+
+                    if (result.box) {
+                        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+                    }
+
+                    if (result.codeResult && result.codeResult.code) {
+                        Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+                    }
+                }
+            });
+
+
+            Quagga.onDetected(function (result) {
+                // console.log("Barcode detected and processed : [" + result.codeResult.code + "]", result);
+				document.getElementById("id").innerHTML = result.codeResult.code;
+				Quagga.stop();
+
+            });
+    });
+</script>
+
 <?php require "templates/footer.php"; ?>
