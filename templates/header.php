d82fcd978b8ae76d8aa25f5d1cf2b207cdc04f67
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-02-17 22:29:29 +0000

    Get rid of scanner for now

--- templates/header.php
+++ templates/header.php
@@ -4,16 +4,6 @@
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<style>
-		/* In order to place the tracking correctly */
-		canvas.drawing, canvas.drawingBuffer {
-			position: absolute;
-			left: 0;
-			top: 0;
-		}
-	</style>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.min.js"></script>
     <title>MRT 3216 Attendance</title>
 
     <link rel="stylesheet" href="css/style.css" />
